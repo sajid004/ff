cea8dc3d364f23aedf3e7317bf4ae5de4c8ad320
sajid004 <hafizsajid.645@gmail.com>
2017-05-17 17:56:17 +0500

    c++ program
    store minimum value

---
+++ ar.mini.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a[10],maini;
+    for(int i=1;i<5;i++)
+    {
+            cin>>a[i];
+            }
+           
+            for(int i=1;i<5;i++)
+    {
+            if(maini>a[i])
+            maini=a[i];
+            }
+            cout<<"maximum no is"<<maini;
+            
+getch();
+}
